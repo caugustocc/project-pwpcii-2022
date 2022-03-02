ad38141d83bf9b8d8001da140e2b2454ce2556af
caugustocc <cronuz300@mail.com>
2022-03-02 07:57:53 -0600

    Adding Babel wathc

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(_, res) {
-  let emojiDataset = ['☠','😀','🤐','👌','👍','🎂','🤷‍♂️','🎶','🌹'];
+  let emojiDataset = ['☠','😀','🤐','👌','👍','🎂','🤷‍♂️','🎶','🌹','🕳'];
 let emojiDb = emojiDataset[Math.floor(Math.random() * emojiDataset.length)];
   //render manda a generar y entregar 
   //la vista al cliente
