10c1fc45b50df0b3a35f4de9cfcb4f2b80af39b1
caugustocc <cronuz300@mail.com>
2022-03-23 08:30:53 -0600

    Actualización webpack Moviendo public

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //middlewere de archivos estaticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,"..",'public')));
 
 
 app.use('/', indexRouter);
