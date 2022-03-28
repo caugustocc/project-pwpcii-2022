bb137f0ccde2ca9315f4543732a87a55e01f1a79
caugustocc <cronuz300@mail.com>
2022-03-28 08:52:44 -0600

    Babel & CSS extract sin probar

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,57 @@
 // Importando dependecia path
- const path = require ('path');
+const path = require('path');
+//plugin de webpack
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     // paso 1 -> especificar el archivo de entrada
     entry: './client/index.js',
     // pas 2 -> especificar el archivo de salida
-    output:{
+    output: {
         // 2.1 requiere la ruta absoluta de la salida
-        path: path.resolve(__dirname,'public'),
+        path: path.resolve(__dirname, 'public'),
         //2.2 Nombre del archivo de salida
         // para poder ser multiplataforma es necesario concatenar las
         // rutas con path
-        filename: path.join('javascript','bundle.js'),
+        filename: path.join('javascript', 'bundle.js'),
         // 2.3 path publico 
         publicPath: '/',
-    }
-}
+    },
+    // 3. modulos
+    module: {
+        rules: [
+            //3.1 Esta es la primera regla de balbel
+            {
+                // en todos los archivos con terminacion JS se aplique esta regla
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    // 3.1.1 primer stage (configurador)
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env', {
+                                        module: false,
+                                        useBuiltIns: 'usage',
+                                        targets: '> 0.25%, not dead',
+                                        corjs: 3
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            },
+            // 3.2 Reglas para CSS
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader]
+            }
+        ]
+    },
+    // 4. plugins
+    Plugins: [new MiniCssExtractPlugin({
+        filename:path.join('stylesheets', 'styles.css')
+    })]
+};
--- webpack.dev.config .js
+++ webpack.dev.config .js
@@ -12,6 +12,7 @@ module.exports = {
         // 2.3 path publico 
         publicPath: '/',
     },
+    
     //configurando el servidor de desarrollo
     devServer: {
         //3.1 folder de archivos estaticos
@@ -22,5 +23,34 @@ module.exports = {
         port: 8080,
         //3.3 definiendo host
         host:'localhost'
+    },
+    // 4. modulos
+    module: {
+        rules: [
+            //4.1 Esta es la primera regla de balbel
+            {
+                // en todos los archivos con terminacion JS se aplique esta regla
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    // 4.1.1 primer stage (configurador)
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env', {
+                                        module: false,
+                                        useBuiltIns: 'usage',
+                                        targets: '> 0.25%, not dead',
+                                        corjs: 3
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
