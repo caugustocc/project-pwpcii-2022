8a472bdc6757c17700b1619fbf6b9fe04264e766
caugustocc <cronuz300@mail.com>
2022-03-28 07:57:51 -0600

    Se agrega config webpack para produccion

--- webpack.dev.config .js
+++ webpack.dev.config .js
@@ -8,10 +8,19 @@ module.exports = {
         // 2.1 requiere la ruta absoluta de la salida
         path: path.resolve(__dirname,'public'),
         //2.2 Nombre del archivo de salida
-        // para poder ser multiplataforma es necesario concatenar las
-        // rutas con path
         filename: path.join('javascript','bundle.js'),
         // 2.3 path publico 
         publicPath: '/',
+    },
+    //configurando el servidor de desarrollo
+    devServer: {
+        //3.1 folder de archivos estaticos
+        static: path.join(__dirname,'public'),
+        // 3.2 puerto del servidor de desarrollo
+        // de WP (Webpack)
+        //en dado caso que este ocupado podemos cambiaro a otro puerto
+        port: 8080,
+        //3.3 definiendo host
+        host:'localhost'
     }
 }
