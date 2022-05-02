d1d33d5d91cb34507aca245e7c5df22ef751a62e
caugustocc <cronuz300@gmail.com>
2022-05-02 07:52:09 -0500

    S28 EBnB configurado ok

--- client/index.js
+++ client/index.js
@@ -1,25 +1,30 @@
 // const { resolve } = require("core-js/fn/promise");
 // const async = require("hbs/lib/async");
 // const { CompileShallowModuleMetadata } = require("prettier");
-console.log("webpack💫");
+/* eslint-disable*/
+/* eslint-disable no console */
+// import ' ./styles/style.css';
+// import ' ./styles/mystyle.css';
+
+console.log('webpack💫');
 // default parameters
-let show = (m ="holis☠")=>{
-    alert(m);
+const show = (m = 'holis ☠') => {
+  alert(m);
 };
 show();
 
 // promises
-function resolveAfter2Seconds(){
-    return new Promise ((resolve) =>{
-        setTimeout(()=>{
-            resolve("functio resolve")  
-        },2000)
-    })
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('functio resolve');
+    }, 2000);
+  });
 }
 
-async function asyncCall(){
-    console.log("calling asyn funtion!!!");
-    const result = await resolveAfter2Seconds();
-    console.log(result);//imprime "funcion resolve" en la consola
+async function asyncCall() {
+  console.log('calling asyn funtion!!!');
+  const result = await resolveAfter2Seconds();
+  console.log(result); // imprime "funcion resolve" en la consola
 }
 asyncCall();
